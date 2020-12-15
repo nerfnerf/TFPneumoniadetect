01e5e59d2283ea80e0296a39d5ed2a343591601a
nerfnerf <flthingdubman@gmail.com>
2020-12-15 19:24:08 +0100

    loading Dataset = shuffle

--- TF_pneumonia.py
+++ TF_pneumonia.py
@@ -11,19 +11,19 @@ from tensorflow.keras.models import Sequential
 
 data_dir = pathlib.Path("/run/user/1000/gvfs/smb-share:server=tower.local,share=coding/chest_xray/chest_xray/train")
 
-train_normal = list(data_dir.glob('train/NORMAL/*.jpeg'))
+#train_normal = list(data_dir.glob('train/NORMAL/*.jpeg'))
 
 
 #image = PIL.Image.open(str(train_normal[0]))
 #image.show()
 
-amount = len(train_normal)
+#amount = len(train_normal)
 
-print(amount)
+#print(amount)
 
 batch_size = 32
-img_height = 180
-img_width = 180
+img_height = 500
+img_width = 500
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
@@ -32,3 +32,26 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.1,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+
+print(np.min(first_image), np.max(first_image))
+
+print("Done")
